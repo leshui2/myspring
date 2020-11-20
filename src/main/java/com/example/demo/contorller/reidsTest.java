86c1603baf4a70e5d26584013edc2214a9d7d060
wxg <484328449@qq.com>
2020-11-20 18:35:57 +0800

    first2

--- src/main/java/com/example/demo/contorller/reidsTest.java
+++ src/main/java/com/example/demo/contorller/reidsTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class reidsTest {
+
     @Autowired
     private RedisTemplate<String,String> redisTemplate;
     @Test
