f3c1fa555f8f10e581cc49d71ec21eef02d128e0
leshui2 <1850700120@qq.com>
2020-11-20 18:56:39 +0800

    first2

--- src/main/java/com/example/demo/contorller/reidsTest.java
+++ src/main/java/com/example/demo/contorller/reidsTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class reidsTest {
 
+
     @Autowired
     private RedisTemplate<String,String> redisTemplate;
     @Test
